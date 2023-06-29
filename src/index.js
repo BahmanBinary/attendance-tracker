ba918c43e91706f6964bd25c6f29be5e6480d993
Bahman Binary <bahman.binary@gmail.com>
2023-06-29 12:00:17 +0330

    Add base layouts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: { fontFamily: "IRANSans" },
+        components: { Layout: { colorBgHeader: "#2196F3" } },
+      }}
+      direction="rtl"
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
